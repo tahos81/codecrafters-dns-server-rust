108c92d071f5499e2e60003b8e31c4cd3ca1b227
tahos81 <tahirozpala@gmail.com>
2024-01-26 13:46:37 +0300

    feat: send logical response

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,26 @@
 use std::net::UdpSocket;
 
-fn main() {
+use crate::{
+    parser::parse_dns_header,
+    types::header::{DnsHeader, DnsHeaderStruct},
+};
+use anyhow::Result;
+
+mod parser;
+mod types;
+
+fn main() -> Result<()> {
     let udp_socket = UdpSocket::bind("127.0.0.1:2053").expect("Failed to bind to address");
-    let mut buf = [0; 512];
 
     loop {
+        let mut buf = [0; 512];
         match udp_socket.recv_from(&mut buf) {
             Ok((size, source)) => {
                 println!("Received {} bytes from {}", size, source);
-                let response = [];
+                let (_, dns_header) = parse_dns_header(&buf).map_err(|err| err.to_owned())?;
+                let response = DnsHeaderStruct::builder(dns_header.id).qr(1).build();
                 udp_socket
-                    .send_to(&response, source)
+                    .send_to(&DnsHeader::from(response), source)
                     .expect("Failed to send response");
             }
             Err(e) => {
@@ -19,4 +29,6 @@ fn main() {
             }
         }
     }
+
+    Ok(())
 }
